cb307c3605506b837bbfc8a3cf99ba6a66795e66
miritru <mtrujillo272@alumno.uned.es>
2025-07-04 22:53:06 +0200

    config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ TOKEN = '7599278607:AAHZegoWRfJogoYyE4sCHIFeL1VlFTEoWaI'
 PLANES_FILE = 'planes.json'
 
 # Lista de usernames permitidos (sin @)
-USUARIOS_PERMITIDOS = ['mirianconn', 'username2']
+USUARIOS_PERMITIDOS = ['mirianconn', 'Mirfraper']
